ae139cb344433311e32b4b57a300823ed2c64d7f
Jon Karna <ipg49vv2@gmail.com>
2012-01-06 11:18:06 -0600

    Changed default namespace to NFL::LiveUpdate

--- lib/nfl/live_update.rb
+++ lib/nfl/live_update.rb
@@ -1,19 +1,20 @@
 require 'json'
 require 'nokogiri'
 require 'open-uri'
-require 'rss'
 
 require './lib/nfl/live_update/score_strip'
 require './lib/nfl/live_update/game_center'
 
 module NFL
+  module LiveUpdate
 
-  def self.score_strip
-    NFL::LiveUpdate::ScoreStrip::Games.current
+    def self.score_strip
+      NFL::LiveUpdate::ScoreStrip::Games.current
+    end
+
+    def self.test_game
+      NFL::LiveUpdate::GameCenter::Game.test_game
+    end
+
   end
-
-  def self.test_game
-    NFL::LiveUpdate::GameCenter::Game.test_game
-  end
-
 end
