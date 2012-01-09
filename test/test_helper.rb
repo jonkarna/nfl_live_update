1816bb6ecfe1828c47dc77fb46d1b394f2d8a6f3
Jon Karna <ipg49vv2@gmail.com>
2012-01-08 19:41:54 -0600

    Added VCR config and fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,12 @@ require 'shoulda'
 require 'turn'
 require 'mocha'
 require 'nfl/live_update'
+require 'vcr'
+require 'webmock'
+
+VCR.config do |c|
+  c.cassette_library_dir = "test/fixtures/vcr_cassettes"
+  c.stub_with :webmock
+  c.allow_http_connections_when_no_cassette = false
+  c.default_cassette_options = {:record => :once}
+end
